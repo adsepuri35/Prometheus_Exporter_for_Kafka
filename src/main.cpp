45bd142222f83780b74d983584926999d901cbe6
Advait Sepuri <90327626+adsepuri35@users.noreply.github.com>
2024-12-26 00:09:34 -0500

    gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int main() {
     socket.bind("tcp://*:5555");
 
     std::cout << "Sending messages..." << std::endl;
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < 100; ++i) {
         trackAndSendMessage(socket, "Hello, World!");
         std::cout << "Sent message " << (i + 1) << std::endl;
     }
